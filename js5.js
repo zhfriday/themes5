eba13245711d337ecb5a66340cc4d0ddaef5f315
zhfriday <zhsummer@ya.ru>
2018-10-11 15:35:41 +0300

    oop

---
+++ js5.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class auto
+
+
+class  Themes extends Course{
+
+}
+
+
+class Question {
+    question: ["первый вопрос?", "второй вопрос?", "третий вопроc?"];
+    answer: ["a", "b","c"];
+
+
+}
