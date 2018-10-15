24a4d22bbf977c8a6fd2f54b96db10a3bbd09bce
zhfriday <zhsummer@ya.ru>
2018-10-15 17:13:19 +0300

    some text

--- js5.js
+++ js5.js
@@ -1,15 +1,55 @@
 "use strict";
+class Course {
+    constructor () {
+        this.type = "Курс Front-end";
 
-class auto {
-    engine = "1,4";
-
-
-}
-
-
-class  bmw extends auto{
-
-
+    }
 
 }
+class Theme extends Course{
+    constructor(themeName, type) {
+        super(type);
+        this.themeName = themeName;
+        this.themes = ["oop", "js", "bootstrap"];
+
+    }
+
+    method() {
+        let themesName = this.themes.find((theme) => {
+            return (theme === this.themeName);
+        });
+            console.log(themesName);
+    }
+
+}
+class Answer {
+    constructor () {
+        this.answers = ["a", "b", "c"];
+    }
+
+    }
+
+class Question {
+    constructor () {
+        this.questionOop = ["вопрос первый?", "вопрос второй?", "вопрос третий?"];
+    }
+
+
+methodOop() {
+    this.questionOop.forEach((question, index) => {
+        let quest = question;
+        alert(quest);
+        console.log(Answer.answers);
+    });
+}}
+
+
+
+
+let oops = new Theme("oop");
+oops.method();
+console.log(oops.type);
+
+let qo = new Question();
+qo.methodOop();
 
