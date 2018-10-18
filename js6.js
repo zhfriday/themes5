5283e48b03d215c348e56bda42ff2f476b6f66ea
zhfriday <zhsummer@ya.ru>
2018-10-18 15:02:31 +0300

    oop

--- js6.js
+++ js6.js
@@ -1,28 +1,132 @@
 "use strict";
-const Devs = [];
-const Developers = 1;
-let type = "mobile";
+// const devsWeb = [];
+// const devsMobile = [];
+const projectArray = [];
+
+
+
+const renderProject = Math.floor(Math.random() * 5);
+const mobileProj = Math.floor(Math.random() * renderProject);
+const webProj = renderProject - mobileProj;
+const difficult = Math.floor((Math.random() * 3)+1);
+
+// const developers = 1;
+//
+//
+// const type = "days";
+
+
+// function typeGeneration () {
+//   let typeCount =  Math.floor(Math.random() * 2);
+//   if (typeCount === 0) {
+//       return "mobile";
+//   } else {
+//       return "web";
+//   }
+// }
+//
+// let typers = typeGeneration();
+// console.log(typers);
+
 
 class Director {
     constructor() {
-        this.renderProject = Math.floor(Math.random() * 5);
+
     }
 
-    method() {
-        for (let i = Developers; i < this.renderProject; i++) {
-            Devs.push(new Developer(type));
+    // method() {
+    //     for (let i = developers; i < this.renderProject; i++) {
+    //         devsMobile.push(new MobileDeveloper(type));
+    //         devsWeb.push(new WebDeveloper(type));
+    //     }
+    // }
+}
+
+
+class Projects extends Director {
+    constructor (types, ) {
+        super(types, renderProject);
+        this.types=types;
+
+
+    }
+
+    methodProj() {
+        for (let index = 0; index < mobileProj; index++) {
+            projectArray.push(new MobileProjects());
+
         }
+        for (let index2 = 0; index2 < webProj; index2++) {
+            projectArray.push(new WebProjects());
+
+        }
+
+        //
+        console.log(`всего проектов = ${renderProject }`);
+        console.log(`мобильных проектов = ${mobileProj}`);
+        console.log(`веб проектов = ${webProj}`);
+    }
+
+
+}
+
+class MobileProjects extends Projects {
+constructor (types){
+    super(types);
+    this.types = "mobileproj";
+    // this.difficult = difficult;
+
+}
+}
+
+
+class WebProjects extends Projects {
+    constructor (types){
+        super(types);
+        this.types = "webproj";
+        // this.difficult = difficult;
     }
 }
 
-class Developer {
-    constructor() {
-        this.type = type;
-    }
-}
 
-let managers = new Director();
-managers.method();
+let pr = new Projects();
+pr.methodProj();
+
+console.log( projectArray);
+// let it = new Projects();
+// it.methodProj();
+// console.log(projectArray);
 
 
-console.log(Devs);
+// class Developer {
+//     constructor(type) {
+//         this.type = type;
+//     }
+// }
+//
+//
+// class MobileDeveloper extends Developer {
+//     constructor(type, gen) {
+//         super(type);
+//         this.type = type;
+//         this.gen = "mobile";
+//
+//     }
+//
+// }
+//
+// class WebDeveloper extends Developer {
+//     constructor(type) {
+//         super(type);
+//         this.type = type;
+//         this.gen = "web";
+//     }
+//
+// }
+
+// let managers = new Director();
+// managers.method();
+
+
+// console.log(devsMobile);
+// console.log(devsWeb);
